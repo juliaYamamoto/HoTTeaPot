a8cdbba40048ff91f56020cdc334a467d9faf16f
Julia <juliaYamamoto@users.noreply.github.com>
2020-12-15 19:03:13 -0800

    Unit test for the AllStatusCode class

--- HTTeaPot/HTTeaPotTests/Mocks/TestObject.swift
+++ HTTeaPot/HTTeaPotTests/Mocks/TestObject.swift
@@ -8,26 +8,24 @@
 import Foundation
 @testable import HTTeaPot
 
-class TestObject {
+// MARK: - Fake Status Code
     
-    // MARK: - Status Code Fakes
-    
-    public let statusCode100 = StatusCode(code: "100", title: "continue", description: "description 100", type: "1xx informational", source: "source 100")
-    public let statusCode101 = StatusCode(code: "101", title: "switching protocols", description: "description 101", type: "1xx informational", source: "source 101")
-    public let statusCode102 = StatusCode(code: "102", title: "processing", description: "description 102", type: "1xx informational", source: "source 102")
+public let fakeStatusCode100 = StatusCode(code: "100", title: "continue", description: "description 100", type: "1xx informational", source: "source 100")
+public let fakeStatusCode101 = StatusCode(code: "101", title: "switching protocols", description: "description 101", type: "1xx informational", source: "source 101")
+public let fakeStatusCode102 = StatusCode(code: "102", title: "processing", description: "description 102", type: "1xx informational", source: "source 102")
 
-    public let statusCode200 = StatusCode(code: "200", title: "OK", description: "description 200", type: "2xx success", source: "source 200")
-    public let statusCode201 = StatusCode(code: "201", title: "created", description: "description 201", type: "2xx success", source: "source 201")
-    public let statusCode202 = StatusCode(code: "202", title: "accepted", description: "description 202", type: "2xx success", source: "source 202")
+public let fakeStatusCode200 = StatusCode(code: "200", title: "OK", description: "description 200", type: "2xx success", source: "source 200")
+public let fakeStatusCode201 = StatusCode(code: "201", title: "created", description: "description 201", type: "2xx success", source: "source 201")
+public let fakeStatusCode202 = StatusCode(code: "202", title: "accepted", description: "description 202", type: "2xx success", source: "source 202")
 
-    public let statusCode300 = StatusCode(code: "300", title: "multiple choices", description: "description 300", type: "3xx redirection", source: "source 300")
+public let fakeStatusCode300 = StatusCode(code: "300", title: "multiple choices", description: "description 300", type: "3xx redirection", source: "source 300")
 
-    public let statusCode400 = StatusCode(code: "400", title: "bad request", description: "description 400", type: "4xx client error", source: "source 400")
-    public let statusCode418 = StatusCode(code: "418", title: "I'm a teapot", description: "description 418", type: "4xx client error", source: "source 418")
+public let fakeStatusCode400 = StatusCode(code: "400", title: "bad request", description: "description 400", type: "4xx client error", source: "source 400")
+public let fakeStatusCode418 = StatusCode(code: "418", title: "I'm a teapot", description: "description 418", type: "4xx client error", source: "source 418")
 
-    public let statusCode500 = StatusCode(code: "500", title: "internal server error", description: "description 500", type: "5xx client error", source: "source 500")
-    
-    
-    // MARK: - All Status Code Fake
-    public let allStatusCode = AllStatusCode(statusCode: [TestObject().statusCode100, TestObject().statusCode101, TestObject().statusCode102, TestObject().statusCode200, TestObject().statusCode201, TestObject().statusCode202, TestObject().statusCode300, TestObject().statusCode400, TestObject().statusCode418, TestObject().statusCode500])
-}
+public let fakeStatusCode500 = StatusCode(code: "500", title: "internal server error", description: "description 500", type: "5xx server error", source: "source 500")
+
+
+// MARK: - Fake All Status Code Fake
+public let fakeAllStatusCode = AllStatusCode(statusCode: [fakeStatusCode100, fakeStatusCode101, fakeStatusCode102, fakeStatusCode200, fakeStatusCode201, fakeStatusCode202, fakeStatusCode300, fakeStatusCode400, fakeStatusCode418, fakeStatusCode500])
+
--- HTTeaPot/HTTeaPotTests/Test Models/AllStatusCodeTest.swift
+++ HTTeaPot/HTTeaPotTests/Test Models/AllStatusCodeTest.swift
@@ -11,15 +11,24 @@ import XCTest
 class AllStatusCodeTest: XCTestCase {
 
     // MARK: - Properties
+    
     var sut: AllStatusCode!
     
+    
     // MARK: - Mock Properties
+    
+    let numberOfelements: Int = 10
+    let numberOfelementsOfTypeInformational: Int = 3
+    let numberOfelementsOfTypeSuccess: Int = 3
+    let numberOfelementsOfTypeRedirection: Int = 1
+    let numberOfelementsOfTypeClientError: Int = 2
+    let numberOfelementsOfTypeServerError: Int = 1
+    
     
     // MARK: - Set up and Tear down
     override func setUp() {
         super.setUp()
-    
-        
+        sut = fakeAllStatusCode
     }
     
     override func tearDown() {
@@ -27,6 +36,38 @@ class AllStatusCodeTest: XCTestCase {
     }
     
     // MARK: - Test: Initialization
+    func testInit_ShouldNotBeNil() {
+        XCTAssertNotNil(sut)
+    }
+    
+    func testInit_StatusCodeArrayShouldNotBeNil() {
+        XCTAssertNotNil(sut.statusCode)
+    }
+    
+    func testInit_ShouldInitializeWith_CorrectCountForElements() {
+        XCTAssertEqual(sut.statusCode.count, numberOfelements)
+    }
     
     
+    // MARK: - Test: getAllOfType()
+    
+    func testInit_ShouldGet_CorrectCountForElementsOfTypeInformational() {
+        XCTAssertEqual(sut.getAllOfType("1xx informational").count, numberOfelementsOfTypeInformational)
+    }
+    
+    func testInit_ShouldGet_CorrectCountForElementsOfTypeSuccess() {
+        XCTAssertEqual(sut.getAllOfType("2xx success").count, numberOfelementsOfTypeSuccess)
+    }
+    
+    func testInit_ShouldGet_CorrectCountForElementsOfTypeRedirection() {
+        XCTAssertEqual(sut.getAllOfType("3xx redirection").count, numberOfelementsOfTypeRedirection)
+    }
+    
+    func testInit_ShouldGet_CorrectCountForElementsOfTypeClientError() {
+        XCTAssertEqual(sut.getAllOfType("4xx client error").count, numberOfelementsOfTypeClientError)
+    }
+    
+    func testInit_ShouldGet_CorrectCountForElementsOfTypeServerError() {
+        XCTAssertEqual(sut.getAllOfType("5xx server error").count, numberOfelementsOfTypeServerError)
+    }
 }
